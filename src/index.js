95a6dc775b28abf18e2256e720e7bc984f0403d8
Bruno Glauco Eles <32398104+bgeles@users.noreply.github.com>
2021-02-18 14:48:11 -0300

    String Interpolation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = "Angela";
-const number = 7;
+const fName = "Bruno";
+const lName = "Eles";
+const number = Math.floor(Math.random() * 10);
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
     <p>Your lucky number is {number}</p>
   </div>,
   document.getElementById("root")
